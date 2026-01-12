b37f889992ee3181823a2ad393271aa9d234090a
jankarlox94 <giancarlosanchez.dev@icloud.com>
2026-01-12 05:49:05 -0600

    Updated text-outline-black class . Added new Quality section

--- src/components/QualityCraftsmanship/QualityCraftsmanship.jsx
+++ src/components/QualityCraftsmanship/QualityCraftsmanship.jsx
@@ -3,6 +3,7 @@ import BowlOne from "../../assets/rice-pudding-bowl.png";
 import BowlTwo from "../../assets/bowl-4.png";
 import RealIngredients from "../../assets/real-ingredients.png";
 import RealToppings from "../../assets/toppings-img.png";
+import OrderPlatform from "../../assets/order-platform.png";
 
 // Placeholder images for the example. You should replace these with your actual image URLs.
 const handCraftedImage = BowlOne;
@@ -21,17 +22,17 @@ const QualityCraftsmanship = () => {
             {/* Card 1: Made on demand */}
             <div className="flex flex-col bg-terracotta sm:flex-row  rounded-2xl p-6 items-start">
               <img
-                src={handCraftedImage}
+                src={OrderPlatform}
                 alt="Hand-Crafted Daily"
                 className="w-full sm:w-32 sm:h-32 rounded-lg object-cover mb-4 sm:mb-0 sm:mr-6"
               />
               <div>
                 <h3 className="text-xl font-semibold mb-2">
-                  Hand-Crafted Daily
+                  Made On-Demand/ Pre-Order
                 </h3>
                 <p className="text-gray-300 leading-relaxed">
-                  Our pudding is made fresh daily in small batches, ensuring
-                  optimal texture and flavor in every cup.
+                  Our pudding is made on-demand, guaranteeing you get it as
+                  fresh as it gets.
                 </p>
               </div>
             </div>
